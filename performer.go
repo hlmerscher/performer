18229057eeb83a19b43f21d4a59b625be420be45
Hercules Merscher <hlmerscher@gmail.com>
2021-05-11 23:37:05 +0200

    package comments

--- performer.go
+++ performer.go
@@ -1,7 +1,10 @@
+// Performer intends to be an alternative to the multiple conditionals spread through the code, providing a cleaner way to handle errors.
 package performer
 
+// Task is the contract that should be implemented by your function(s) (or methods).
 type Task func() error
 
+// Do is the function that will perform the tasks. Given one or multiple tasks, they will be performed one by one. When faced with an error, the execution is halted and error is returned.
 func Do(iterator ...Task) error {
 	for _, fn := range iterator {
 		if err := fn(); err != nil {
